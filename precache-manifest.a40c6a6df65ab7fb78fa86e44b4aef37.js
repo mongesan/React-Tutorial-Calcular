ead2690924a783dccb51d0a99de56376e866c251
mongesan <syun1.mongesan@gmail.com>
2020-04-30 20:38:03 +0900

    Updates

---
+++ precache-manifest.a40c6a6df65ab7fb78fa86e44b4aef37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d7cca423734f352383f03df1cbc023",
+    "url": "/React-Tutorial-Calcular/index.html"
+  },
+  {
+    "revision": "a3db9de6de8997058a60",
+    "url": "/React-Tutorial-Calcular/static/js/2.895474df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Tutorial-Calcular/static/js/2.895474df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e39ae89d25ae03ab5e",
+    "url": "/React-Tutorial-Calcular/static/js/main.d48db6a9.chunk.js"
+  },
+  {
+    "revision": "e313ba8bf3e630afd9a0",
+    "url": "/React-Tutorial-Calcular/static/js/runtime-main.b2602ee5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Tutorial-Calcular/precache-manifest.a40c6a6df65ab7fb78fa86e44b4aef37.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Tutorial-Calcular/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
